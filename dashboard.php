0795c94ae69b1def1fe0e1733526a2389e512f81
CoD3r ( <144725288+g7t2024@users.noreply.github.com>
2023-11-21 13:13:44 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello, World!</title>
+    <link rel="stylesheet" href="dashboard.css">
+    <title>Dashboard</title>
 </head>
 <body>
-    <h1>Hello, World!</h1>
+    <div class="dashboard-container">
+        <h1>Bem-vindo ao painel!</h1>
+        <a href="logout.php">Logout</a>
+    </div>
 </body>
 </html>
