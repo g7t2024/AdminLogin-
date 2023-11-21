6d59e8ea4131f1224f1e5943b91624be76901990
CoD3r ( <144725288+g7t2024@users.noreply.github.com>
2023-11-21 12:54:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     const password = document.getElementById("password").value;
 
     // Substitua esta lógica por autenticação real
-    if (username === "usuario" && password === "senha") {
+    if (username === "admin" && password === "root") {
         alert("Login bem-sucedido!");
         // Redirecionar ou realizar outras ações após o login
     } else {
